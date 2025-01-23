60486963a87f6ed83ee42a0aa170fac3c35e6663
zacklow28 <zo.ngyun@hotmail.com>
2025-01-23 16:08:27 +0800

    level0

--- src/main/java/FeedMe.java
+++ src/main/java/FeedMe.java
@@ -1,11 +1,8 @@
 public class FeedMe {
     public static void main(String[] args) {
-        String message = "____________________________________________________________\n" +
-                " Hello! I'm FeedMe.\n" +
+        String message =  "Hello! I'm FeedMe.\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+                " Bye. Hope to see you again soon!\n";
         System.out.println(message);
     }
 }
