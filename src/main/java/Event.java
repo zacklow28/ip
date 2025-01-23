f0380b66ab85ed1e9dccd76271049b5991dbf312
zacklow28 <zo.ngyun@hotmail.com>
2025-01-23 22:01:52 +0800

    Bug Fix

---
+++ src/main/java/Event.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Event extends Task {
+    private String startdate;
+    private String enddate;
+
+    public Event(String name, String date) {
+        super(name);
+        this.startdate = date.split(" /to ")[0];
+        this.enddate = date.split(" /to ")[1];
+    }
+
+    public String getStartDate() {
+        return this.startdate;
+    }
+
+    public String getEndDate() {
+        return this.enddate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.startdate, this.enddate);
+    }
+}
