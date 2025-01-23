c46ee57ace0859fd4afcda7065dc1730fa1e4e02
zacklow28 <zo.ngyun@hotmail.com>
2025-01-23 16:55:49 +0800

    level2

--- src/main/java/FeedMe.java
+++ src/main/java/FeedMe.java
@@ -10,13 +10,24 @@ public class FeedMe {
         System.out.println(greeting);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String in = br.readLine();
+        ArrayList<String> list = new ArrayList<String>();
 
-        // loop and echo content until user types "bye"
-        while (!in.equals("bye")) {
-            System.out.println(in);
+        // loop and add content to list until user types "bye"
+        while (true) {
+            if (in.equals("bye")) {
+                break;
+            }
+            if (in.equals("list")) {
+                int[] iteration = {1};
+                //use iteration[0], then increment it for each element
+                list.forEach(x -> System.out.println((iteration[0]++) + ": " + x));
+            }
+            else {
+                list.add(in);
+                System.out.println("added: " + in);
+            }
             in = br.readLine();
         }
         System.out.println(goodbye);
-
     }
 }
