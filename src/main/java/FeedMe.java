bb8d4a7cae9be1fc73095fe3d4695e71939426e9
zacklow28 <zo.ngyun@hotmail.com>
2025-01-23 23:12:05 +0800

    level5

--- src/main/java/FeedMe.java
+++ src/main/java/FeedMe.java
@@ -10,12 +10,13 @@ public class FeedMe {
         System.out.println(greeting);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String in = br.readLine();
-        ArrayList<Task> tasklist = new ArrayList<Task>();
+        ArrayList<Task> tasklist = new ArrayList<>();
 
         // loop
         while (in != null) {
             String[] inArr = in.split(" ", 2);  //split just on the first space
             if (inArr[0].equals("bye")) {
+                System.out.println(goodbye);
                 break;
             }
             else if (inArr[0].equals("list")) {
@@ -25,37 +26,77 @@ public class FeedMe {
                 }
             }
             else if (inArr[0].equals("mark")) {
-                int index = Integer.parseInt(inArr[1]) - 1;
-                Task curr = tasklist.get(index);
-                curr.mark();
-                System.out.println("Nice! I've marked this task as done!\n" + curr.toString());
+                try {
+                    int index = Integer.parseInt(inArr[1]) - 1;
+                    Task curr = tasklist.get(index);
+                    curr.mark();
+                    System.out.println("Nice! I've marked this task as done!\n" + curr);
+                }
+                catch (IndexOutOfBoundsException e) {
+                    System.out.println("Invalid/Missing Task Number! Please try again.");
+                    in = br.readLine();
+                    continue;
+                }
             }
             else if (inArr[0].equals("unmark")) {
-                int index = Integer.parseInt(inArr[1]) - 1;
-                Task curr = tasklist.get(index);
-                curr.unmark();
-                System.out.println("Okay! I've marked this task as not done!\n" + curr.toString());
+                try {
+                    int index = Integer.parseInt(inArr[1]) - 1;
+                    Task curr = tasklist.get(index);
+                    curr.unmark();
+                    System.out.println("Okay! I've marked this task as not done!\n" + curr);
+                }
+                catch (IndexOutOfBoundsException e) {
+                    System.out.println("Invalid/Missing Task Number! Please try again.");
+                    in = br.readLine();
+                    continue;
+                }
             }
             else {
-                Task curr = new Task(in);
+                Task curr;
                 if (inArr[0].equals("todo")) {
-                    curr = new ToDo(inArr[1]);
+                    try {
+                        curr = new ToDo(inArr[1]);
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Missing Task Name! Please try again.");
+                        in = br.readLine();
+                        continue;
+                    }
                 }
                 else if (inArr[0].equals("deadline")) {
-                    String[] inArr2 = inArr[1].split(" /by "); //split second part to get due date
-                    curr = new Deadline(inArr2[0], inArr2[1]);
+                    try {
+                        String[] inArr2 = inArr[1].split(" /by "); //split second part to get due date
+                        curr = new Deadline(inArr2[0], inArr2[1]);
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Missing/Invalid Task Name and/or Date! Please try again.");
+                        in = br.readLine();
+                        continue;
+                    }
                 }
                 else if (inArr[0].equals("event")) {
-                    String[] inArr2 = inArr[1].split(" /from "); //split second part to get due date
-                    curr = new Event(inArr2[0], inArr2[1]);
+                    try {
+                        String[] inArr2 = inArr[1].split(" /from "); //split second part to get due date
+                        curr = new Event(inArr2[0], inArr2[1]);
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Missing/Invalid Task Name and/or Date! Please try again.");
+                        in = br.readLine();
+                        continue;
+                    }
+                }
+                else {
+                    System.out.println("Invalid Command! Please try again.");
+                    in = br.readLine();
+                    continue;
                 }
                 tasklist.add(curr);
-                System.out.println("Got it. I've added this task:\n" + curr.toString());
+                System.out.println("Got it. I've added this task:\n" + curr);
                 String taskword = tasklist.size() == 1 ? "task" : "tasks";
                 System.out.printf("Now you have %s %s in the list.\n", tasklist.size(), taskword);
             }
             in = br.readLine();
         }
-        System.out.println(goodbye);
+        br.close();
     }
 }
