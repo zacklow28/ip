ff0a0c5a3e5f40cd7f1114937f55d5f910abcb1f
zacklow28 <zo.ngyun@hotmail.com>
2025-01-23 16:36:00 +0800

    level1

--- src/main/java/FeedMe.java
+++ src/main/java/FeedMe.java
@@ -1,8 +1,22 @@
+import java.util.*;
+import java.io.*;
+
 public class FeedMe {
-    public static void main(String[] args) {
-        String message =  "Hello! I'm FeedMe.\n" +
-                " What can I do for you?\n" +
-                " Bye. Hope to see you again soon!\n";
-        System.out.println(message);
+    public static void main(String[] args) throws IOException {
+        String greeting =  "Hello! I'm FeedMe.\n" +
+                "What can I do for you?\n";
+        String goodbye = "Bye. Hope to see you again soon!\n";
+
+        System.out.println(greeting);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String in = br.readLine();
+
+        // loop and echo content until user types "bye"
+        while (!in.equals("bye")) {
+            System.out.println(in);
+            in = br.readLine();
+        }
+        System.out.println(goodbye);
+
     }
 }
