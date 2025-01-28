de766683ed589e90632f51c6b0f98e46bdf8ffee
zacklow28 <zo.ngyun@hotmail.com>
2025-01-28 21:55:55 +0800

    added memory feature

--- src/main/java/FeedMe.java
+++ src/main/java/FeedMe.java
@@ -2,19 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class FeedMe {
+    private static final ArrayList<Task> tasklist = new ArrayList<>();
 
     public static void delete(ArrayList<Task> tasklist, int index) {
-        System.out.println("Okay, I've removed this task:\n" + tasklist.get(index));
+        System.out.println("Okay, I've swallowed this Food:\n" + tasklist.get(index));
         tasklist.remove(index);
     }
 
     public static void printTotal(ArrayList<Task> tasklist) {
-        String taskword = tasklist.size() == 1 ? "task" : "tasks";
-        System.out.printf("Now you have %s %s in the list.\n", tasklist.size(), taskword);
+        System.out.printf("Now you have %s Food in my tummy.\n", tasklist.size());
     }
 
     public static void printList(ArrayList<Task> tasklist) {
-        System.out.println("Here are the tasks in your list: ");
+        System.out.println("Here are the Food in my tummy: ");
         for (int i = 1; i <= tasklist.size(); i++) {
             System.out.println(i + ": " + tasklist.get(i - 1).toString());
         }
@@ -22,21 +22,98 @@ public class FeedMe {
 
     public static void addTask(ArrayList<Task> tasklist, Task task) {
         tasklist.add(task);
-        System.out.println("Got it. I've added this task:\n" + task);
+    }
+
+    public static void initializeFile(String filepath) throws IOException {
+        File file = new File(filepath);
+        if (!file.exists()) {
+            file.createNewFile();
+            System.out.println("Created a new stomach at: " + filepath);
+        }
+    }
+
+    public static void retrieveFromFile(String filepath) throws IOException {
+        File file = new File(filepath);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            line = line.replace("to:", "/to");
+            char taskType = line.charAt(1);
+            String food = line.substring(7).trim(); //exclude the [ ][ ]
+            switch (taskType) {
+            case 'T':
+                if (line.contains("[X]")) {
+                    addTask(tasklist, new ToDo(food, true));
+                } else {
+                    addTask(tasklist, new ToDo(food));
+                }
+                break;
+            case 'D':
+                String[] foodArr = food.split(" by: ");
+                if (line.contains("[X]")) {
+                    addTask(tasklist, new Deadline(foodArr[0], true, foodArr[1]));
+                } else {
+                    addTask(tasklist, new Deadline(foodArr[0], foodArr[1]));
+                }
+                break;
+            case 'E':
+                String[] foodArr2 = food.split(" from: ");
+                if (line.contains("[X]")) {
+                    addTask(tasklist, new Event(foodArr2[0], true, foodArr2[1]));
+                } else {
+                    addTask(tasklist, new Event(foodArr2[0], foodArr2[1]));
+                }
+                break;
+            default:
+                throw new IndexOutOfBoundsException();
+            }
+        }
+        scanner.close();
+    }
+
+    public static void appendToFile(String filepath, Task curr) throws IOException {
+        FileWriter fw = new FileWriter(filepath, true);
+        fw.write(curr.toString() + "\n");
+        fw.close();
+    }
+
+    public static void writeToFile(String filepath, ArrayList<Task> tasklist) throws IOException {
+        FileWriter fw = new FileWriter(filepath);
+        for (Task t : tasklist) {
+            fw.write(t.toString() + "\n");
+        }
+        fw.close();
     }
 
     public static void main(String[] args) throws IOException {
         String greeting =  "Hello! I'm FeedMe.\nWhat can I do for you?\n";
-        String goodbye = "Bye. Hope to see you again soon!\n";
-        String invalidCommand = "Invalid/Missing Command! Please try again.\n";
-        String invalidIndex = "Invalid/Missing Task Number! Please try again.\n";
-        String invalidName = "Invalid/Missing Task Name! Please try again.\n";
-        String invalidNameOrDate = "Invalid/Missing Task Name and/or Date! Please try again.\n";
+        String goodbye = "Munch. Hope to see you again soon!\n";
+        String invalidCommand = "Invalid/Missing Command! Feed me again!\n";
+        String invalidIndex = "Invalid/Missing Food Index! Feed me again!\n";
+        String invalidName = "Invalid/Missing Food Name! Feed me again!\n";
+        String invalidNameOrDate = "Invalid/Missing Food Name and/or Date! Feed me again!\n";
 
         System.out.println(greeting);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Tummy path (with extension) : ");
+        String filepath = br.readLine();
+
+        //read from file
+        while (filepath != null) {
+            try {
+                retrieveFromFile(filepath);
+                break;
+            } catch (FileNotFoundException e) {
+                System.out.println("Tummy location not found.");
+                initializeFile(filepath);
+                break;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Food content corrupted! Feed me again!");
+                filepath = br.readLine();
+            }
+        }
+        System.out.println("Tummy set!");
         String in = br.readLine();
-        ArrayList<Task> tasklist = new ArrayList<>();
 
         // loop
         while (in != null) {
@@ -53,6 +130,8 @@ public class FeedMe {
                     int index = Integer.parseInt(inArr[1]) - 1;
                     Task curr = tasklist.get(index);
                     curr.mark();
+                    System.out.println("Yay! I've marked this Food as eaten!\n" + curr);
+                    writeToFile(filepath, tasklist);
                 }
                 catch (IndexOutOfBoundsException e) {
                     System.out.println(invalidIndex);
@@ -65,6 +144,8 @@ public class FeedMe {
                     int index = Integer.parseInt(inArr[1]) - 1;
                     Task curr = tasklist.get(index);
                     curr.unmark();
+                    System.out.println("Aww! I've marked this Food as not eaten!\n" + curr);
+                    writeToFile(filepath, tasklist);
                 }
                 catch (IndexOutOfBoundsException e) {
                     System.out.println(invalidIndex);
@@ -77,6 +158,7 @@ public class FeedMe {
                     int index = Integer.parseInt(inArr[1]) - 1;
                     delete(tasklist, index);
                     printTotal(tasklist);
+                    writeToFile(filepath, tasklist);
                 }
                 catch (IndexOutOfBoundsException e) {
                     System.out.println(invalidIndex);
@@ -124,6 +206,8 @@ public class FeedMe {
                     continue;
                 }
                 addTask(tasklist, curr);
+                System.out.println("Got it. I've added this Food:\n" + curr);
+                appendToFile(filepath, curr);
                 printTotal(tasklist);
             }
             in = br.readLine();
