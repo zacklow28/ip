3be6ff9ab7f6b5e47972d316af60784df3365ff6
zacklow28 <zo.ngyun@hotmail.com>
2025-04-04 20:30:59 +0800

    Add Styling

--- src/main/java/feedme/Main.java
+++ src/main/java/feedme/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            scene.getStylesheets().add(getClass().getResource("/view/MainWindow.css").toExternalForm());
             stage.setTitle("FeedMe");
             stage.setScene(scene);
             stage.show();
